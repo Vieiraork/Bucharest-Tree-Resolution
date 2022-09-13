ae79f600c9ac86547d31236f7199652c0cf360c7
Vieiraork <vieiraork@gmail.com>
2022-09-12 21:47:58 -0300

    Add - config file

---
+++ config.py
@@ -0,0 +1,77 @@
+cities = [
+    {
+        'proxima': 'Zerind',
+        'anterior': 'Arad',
+        'distancia': 75
+    },
+    {
+        'proxima': 'Oradea',
+        'anterior': 'Zerind',
+        'distancia': 71
+    },
+    {
+        'proxima': 'Sibiu',
+        'anterior': 'Oradea',
+        'distancia': 151
+    },
+    {
+        'proxima': 'Sibiu',
+        'anterior': 'Arad',
+        'distancia': 140
+    },
+    {
+        'proxima': 'Fagaras',
+        'anterior': 'Sibiu',
+        'distancia': 99
+    },
+    {
+        'proxima': 'Bucharest',
+        'anterior': 'Fagaras',
+        'distancia': 211
+    },
+    {
+        'proxima': 'Rimnicu',
+        'anterior': 'Sibiu',
+        'distancia': 80
+    },
+    {
+        'proxima': 'Pitesti',
+        'anterior': 'Rimnicu',
+        'distancia': 97
+    },
+    {
+        'proxima': 'Bucharest',
+        'anterior': 'Pitesti',
+        'distancia': 101
+    },
+    {
+        'proxima': 'Timisoara',
+        'anterior': 'Arad',
+        'distancia': 118
+    },
+    {
+        'proxima': 'Lugoj',
+        'anterior': 'Timisoara',
+        'distancia': 111
+    },
+    {
+        'proxima': 'Mehadia',
+        'anterior': 'Lugoj',
+        'distancia': 70
+    },
+    {
+        'proxima': 'Dobreta',
+        'anterior': 'Mehadia',
+        'distancia': 75
+    },
+    {
+        'proxima': 'Craiova',
+        'anterior': 'Dobreta',
+        'distancia': 120
+    },
+    {
+        'proxima': 'Pitesti',
+        'anterior': 'Craiova',
+        'distancia': 138
+    }
+]
